927d8ac4d1a29aa20d9c68423cf08d7279925802
rgyrgjrgk@naver.com <rgyrgjrgk@naver.com>
2023-11-10 17:31:56 +0900

    - implemented scroll view

--- Assets/Editor/TileMapEditor.cs
+++ Assets/Editor/TileMapEditor.cs
@@ -15,23 +15,7 @@ public class TileMapEditor : Editor
         tileMap = target as TileMap;
         Tools.current = Tool.View;
 
-        if(tileMap.texture2D != null)
-        {
-            var path = AssetDatabase.GetAssetPath(tileMap.texture2D);
-            //Debug.Log(path);
-            var array = AssetDatabase.LoadAllAssetsAtPath(path);
-            //Debug.Log(array.Length);
-
-            tileMap.sprites = new Sprite[array.Length - 1];
-            for(int i = 1; i < array.Length; i++)
-            {
-                tileMap.sprites[i - 1] = array[i] as Sprite;
-                //Debug.Log(tileMap.sprites[i - 1].name);
-            }
-            var w = tileMap.sprites[0].textureRect.width;
-            var h = tileMap.sprites[0].textureRect.height;
-            tileMap.tileSize = new Vector2(w,h);
-        }
+        UpdateTileMap();
     }
 
     public void OnDisable() // 인스펙터 비활성화(다른 인스펙터창으로 바뀜)
@@ -49,20 +33,57 @@ public class TileMapEditor : Editor
 
         EditorGUILayout.LabelField("Our custom editor"); // 읽기만 가능
 
+        var prevMapSize = tileMap.mapSize;
         tileMap.mapSize = EditorGUILayout.Vector2Field("Map Size", tileMap.mapSize);
+
+        var prevTexture2D = tileMap.texture2D;
         tileMap.texture2D = EditorGUILayout.ObjectField("Texture2D: ", tileMap.texture2D, typeof(Texture2D), false) as Texture2D;
 
-        if(tileMap.texture2D == null )
+        if(prevMapSize != tileMap.mapSize || prevTexture2D != tileMap.texture2D)
+        {
+            UpdateTileMap();
+        }
+
+        if (tileMap.texture2D == null)
         {
             EditorGUILayout.HelpBox("텍스쳐를 선택하세요.", MessageType.Warning); // MessageType.Warning : 오류 뜸
         }
         else
         {
-            EditorGUILayout.LabelField($"Tile Size: { tileMap.tileSize }");
+            EditorGUILayout.LabelField($"Tile Size: {tileMap.tileSize}");
+            EditorGUILayout.LabelField($"Grid Size: {tileMap.gridSize}"); // 월드에서의 사이즈
+            EditorGUILayout.LabelField($"Pixel To Unit: {tileMap.pixelToUnits}");
         }
 
 
         EditorGUILayout.EndVertical();
         EditorUtility.SetDirty(tileMap); // 해당 게임오브젝트의 TileMap컴포넌트에 값 저장
+    }
+
+    private void UpdateTileMap()
+    {
+        if(tileMap.texture2D == null)
+        {
+            return;
+        }
+        var path = AssetDatabase.GetAssetPath(tileMap.texture2D);
+        //Debug.Log(path);
+        var array = AssetDatabase.LoadAllAssetsAtPath(path);
+        //Debug.Log(array.Length);
+
+        tileMap.sprites = new Sprite[array.Length - 1];
+        for (int i = 1; i < array.Length; i++)
+        {
+            tileMap.sprites[i - 1] = array[i] as Sprite;
+            //Debug.Log(tileMap.sprites[i - 1].name);
+        }
+        var sampleSprite = tileMap.sprites[0];
+        var w = sampleSprite.textureRect.width;
+        var h = sampleSprite.textureRect.height;
+        tileMap.tileSize = new Vector2(w, h);
+        tileMap.pixelToUnits = (int)(w / sampleSprite.bounds.size.x);
+
+        tileMap.gridSize = new Vector2(w * tileMap.mapSize.x, h * tileMap.mapSize.y);
+        tileMap.gridSize /= tileMap.pixelToUnits;
     }
 }
--- Assets/Scripts/TileMap.cs
+++ Assets/Scripts/TileMap.cs
@@ -8,11 +8,34 @@ public class TileMap : MonoBehaviour
     public Texture2D texture2D; // 타일 전체 이미지
     public Vector2 tileSize; // 타일 하나의 사이즈
     public Vector2 tileOffset;
-    public Vector2 tilePadding;
+    public Vector2 tilePadding { get; set; } = new Vector2(2f,2f);
     public Sprite[] sprites; // slice된 타일 하나씩 배열로 추가
     public Vector2 gridSize; // 전체 사이즈
-    public int pizelToUnits;
+    public int pixelToUnits;
 
     public int tileId; // sprites의 index
     public GameObject tiles; // 게임오브젝트 부모 배치용
+
+    private void OnDrawGizmosSelected()
+    {
+        var pos = transform.position;
+        var center = new Vector2(pos.x + gridSize.x * 0.5f, pos.y - gridSize.y * 0.5f);
+        var newCenter = new Vector2(pos.x + tileSize.x * 0.5f, pos.y - tileSize.y * 0.5f);
+        var first = newCenter;
+        Gizmos.color = Color.white;
+        Gizmos.DrawWireCube(center, gridSize);
+
+        Gizmos.color = Color.cyan;
+
+        for (int i = 1; i < mapSize.x; ++i)
+        {
+            var x = pos.x + tileSize.x * i;
+            Gizmos.DrawLine(new Vector2(x, pos.y), new Vector2(x, pos.y - gridSize.y));
+        }
+        for (int i = 1; i < mapSize.y; ++i)
+        {
+            var y = pos.y - tileSize.y * i;
+            Gizmos.DrawLine(new Vector2(pos.x, y), new Vector2(pos.x + gridSize.x, y));
+        }
+    }
 }
