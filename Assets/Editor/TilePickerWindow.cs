e92a9637e5ebe35a6c638fcccac9da10eddc917b
rgyrgjrgk@naver.com <rgyrgjrgk@naver.com>
2023-11-13 15:44:10 +0900

    - Implemented eraiser, brush, clear

--- Assets/Editor/TilePickerWindow.cs
+++ Assets/Editor/TilePickerWindow.cs
@@ -10,13 +10,19 @@ public class TilePickerWindow : EditorWindow
         x1, x2, x3, x4, x5
     }
 
-    Scale scale = Scale.x1;
-    Vector2 scrollPosition;
+    private Scale scale = Scale.x1;
+    private Vector2 scrollPosition;
+    private Vector2Int selectTileIndex;
+    private bool isRepaint = false;
 
     [MenuItem("Window/Tile Picker")]
     public static void Open()
     {
         var window = EditorWindow.GetWindow<TilePickerWindow>(); // 이미 생성되어 있으면 넘겨주고, 없으면 생성해서 넘겨줌
+        window.isRepaint = true;
+        window.selectTileIndex = Vector2Int.zero;
+        TileMap.SelectedTileIndex = 0;
+
         var title = new GUIContent();
         title.text = "Tile Picker";
         window.titleContent = title;
@@ -61,24 +67,39 @@ public class TilePickerWindow : EditorWindow
         var style = new GUIStyle(GUI.skin.customStyles[0]); // 기본값 세팅 후 원하는 것만 바꾸도록
         style.normal.background = boxTexture;
 
-        var currentEvent = Event.current;
-        var mousePos = currentEvent.mousePosition;
-        mousePos.x += scrollPosition.x - offset.x;
-        mousePos.y += scrollPosition.y - offset.y;
-
         var scaledSize = tileMap.tileSize * ((int)scale + 1);
         scaledSize += tileMap.tilePadding * ((int)scale + 1);
 
-        var selectTileIndex = Vector2Int.zero;
-        selectTileIndex.x = Mathf.FloorToInt(mousePos.x / scaledSize.x);
-        selectTileIndex.y = Mathf.FloorToInt(mousePos.y / scaledSize.y);
+        var currentEvent = Event.current;
+        if(currentEvent.type == EventType.MouseDown && currentEvent.button == 0)
+        {
+            var mousePos = currentEvent.mousePosition;
+            mousePos.x += scrollPosition.x - offset.x;
+            mousePos.y += scrollPosition.y - offset.y;
 
-        var highlightRect = 
-            new Rect(selectTileIndex.x * scaledSize.x + offset.x,
-                     selectTileIndex.y * scaledSize.y + offset.y,
-                     scaledSize.x, scaledSize.y);
+            var counts = new Vector2(texture2D.width / (tileMap.tileSize.x + tileMap.tilePadding.x), 
+                                     texture2D.height / (tileMap.tileSize.y + tileMap.tilePadding.y));
+
+            selectTileIndex.x = Mathf.FloorToInt(mousePos.x / scaledSize.x);
+            selectTileIndex.y = Mathf.FloorToInt(mousePos.y / scaledSize.y);
+            selectTileIndex.x = Mathf.Clamp(selectTileIndex.x, 0, (int)counts.x - 1);
+            selectTileIndex.y = Mathf.Clamp(selectTileIndex.y, 0, (int)counts.y - 1);
+
+            TileMap.SelectedTileIndex = selectTileIndex.y * (int)counts.x + selectTileIndex.x;
+
+            isRepaint = true;
+        }
+
+        var highlightRect = new Rect(
+            selectTileIndex.x * scaledSize.x + offset.x,
+            selectTileIndex.y * scaledSize.y + offset.y,
+            scaledSize.x, scaledSize.y);
         GUI.Box(highlightRect, "", style);
-        Repaint();
-        Debug.Log(scrollPosition);
+
+        if(isRepaint)
+        {
+            Repaint();
+            isRepaint = false;
+        }
     }
 }
--- Assets/Scripts/TileMap.cs
+++ Assets/Scripts/TileMap.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class TileMap : MonoBehaviour
 {
+    public static int SelectedTileIndex { get; set; }
+
     public Vector2 mapSize = new Vector2(20, 10);
     public Texture2D texture2D; // 타일 전체 이미지
     public Vector2 tileSize; // 타일 하나의 사이즈
@@ -12,9 +14,6 @@ public class TileMap : MonoBehaviour
     public Sprite[] sprites; // slice된 타일 하나씩 배열로 추가
     public Vector2 gridSize; // 전체 사이즈
     public int pixelToUnits;
-
-    public int tileId; // sprites의 index
-    public GameObject tiles; // 게임오브젝트 부모 배치용
 
     private void OnDrawGizmosSelected()
     {
@@ -37,5 +36,10 @@ public class TileMap : MonoBehaviour
             var y = pos.y - tileSize.y * i;
             Gizmos.DrawLine(new Vector2(pos.x, y), new Vector2(pos.x + gridSize.x, y));
         }
+    }
+
+    public static string GetTileId(int r, int c)
+    {
+        return $"Tile (${r}, {c})";
     }
 }
